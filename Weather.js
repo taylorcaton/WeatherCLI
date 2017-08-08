e79e788e8d48ef7aa91e801d4bd44daf82537e4d
Taylor Caton <TaylorCaton@hgbc.org>
2017-08-08 19:31:01 -0400

    Weather.js

---
+++ Weather.js
@@ -0,0 +1,19 @@
+var weather = require('weather-js');
+
+var Weather = function(name, location){
+    this.name = name;
+    this.location = location;
+    this.getWeather(this.location);
+}
+
+Weather.prototype.getWeather = function(location){
+    weather.find({search: location, degreeType: 'F'}, function(err, result) {
+        if(err) console.log(err);
+        
+        console.log(JSON.stringify(result, null, 2));
+    });
+}
+
+module.exports = Weather;
+
+new Weather("Me", "Apex, NC")
